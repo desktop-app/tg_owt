bc32bec863374ad8acc5ce8e86b5455c38e6514c
gasinvein <gasinvein@gmail.com>
2020-08-22 15:16:06 +0300

    Find deps for packaged build

--- cmake/external.cmake
+++ cmake/external.cmake
@@ -3,6 +3,9 @@ set(TG_OWT_OPENSSL_INCLUDE_PATH "" CACHE STRING "Include path for openssl.")
 
 function(link_openssl target_name)
     if (TG_OWT_PACKAGED_BUILD)
+        find_package(OpenSSL REQUIRED)
+        target_include_directories(${target_name} PUBLIC ${OPENSSL_INCLUDE_DIR})
+        target_link_libraries(${target_name} PUBLIC OpenSSL::Crypto OpenSSL::SSL)
     else()
         if (TG_OWT_OPENSSL_INCLUDE_PATH STREQUAL "")
             message(FATAL_ERROR "You should specify 'TG_OWT_OPENSSL_INCLUDE_PATH'.")
@@ -19,6 +22,10 @@ endfunction()
 set(TG_OWT_OPUS_INCLUDE_PATH "" CACHE STRING "Include path for opus.")
 function(link_opus target_name)
     if (TG_OWT_PACKAGED_BUILD)
+        find_package(PkgConfig REQUIRED)
+        pkg_check_modules(OPUS REQUIRED IMPORTED_TARGET opus)
+        target_include_directories(${target_name} PUBLIC ${OPUS_INCLUDE_DIRS})
+        target_link_libraries(${target_name} PUBLIC PkgConfig::OPUS)
     else()
         if (TG_OWT_OPUS_INCLUDE_PATH STREQUAL "")
             message(FATAL_ERROR "You should specify 'TG_OWT_OPUS_INCLUDE_PATH'.")
@@ -35,6 +42,26 @@ endfunction()
 set(TG_OWT_FFMPEG_INCLUDE_PATH "" CACHE STRING "Include path for ffmpeg.")
 function(link_ffmpeg target_name)
     if (TG_OWT_PACKAGED_BUILD)
+        find_package(PkgConfig REQUIRED)
+        pkg_check_modules(AVCODEC REQUIRED IMPORTED_TARGET libavcodec)
+        pkg_check_modules(AVFORMAT REQUIRED IMPORTED_TARGET libavformat)
+        pkg_check_modules(AVUTIL REQUIRED IMPORTED_TARGET libavutil)
+        pkg_check_modules(SWSCALE REQUIRED IMPORTED_TARGET libswscale)
+        pkg_check_modules(SWRESAMPLE REQUIRED IMPORTED_TARGET libswresample)
+        target_include_directories(${target_name} PUBLIC
+            ${AVCODEC_INCLUDE_DIRS}
+            ${AVFORMAT_INCLUDE_DIRS}
+            ${AVUTIL_INCLUDE_DIRS}
+            ${SWSCALE_INCLUDE_DIRS}
+            ${SWRESAMPLE_INCLUDE_DIRS}
+        )
+        target_link_libraries(${target_name} PUBLIC
+            PkgConfig::AVCODEC
+            PkgConfig::AVFORMAT
+            PkgConfig::AVUTIL
+            PkgConfig::SWSCALE
+            PkgConfig::SWRESAMPLE
+        )
     else()
         if (TG_OWT_FFMPEG_INCLUDE_PATH STREQUAL "")
             message(FATAL_ERROR "You should specify 'TG_OWT_FFMPEG_INCLUDE_PATH'.")
@@ -51,6 +78,9 @@ endfunction()
 set(TG_OWT_LIBJPEG_INCLUDE_PATH "" CACHE STRING "Include path for libjpeg.")
 function(link_libjpeg target_name)
     if (TG_OWT_PACKAGED_BUILD)
+        find_package(JPEG REQUIRED)
+        target_include_directories(${target_name} PUBLIC ${JPEG_INCLUDE_DIR})
+        target_link_libraries(${target_name} PUBLIC JPEG::JPEG)
     else()
         if (TG_OWT_LIBJPEG_INCLUDE_PATH STREQUAL "")
             message(FATAL_ERROR "You should specify 'TG_OWT_LIBJPEG_INCLUDE_PATH'.")
