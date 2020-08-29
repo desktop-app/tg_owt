54f4af0f861987d9064e36515c842d7ee63c2a2f
Ilya Fedin <fedin-ilja2010@ya.ru>
2020-08-29 19:18:51 +0400

    Specify some flags only on special target

--- cmake/init_target.cmake
+++ cmake/init_target.cmake
@@ -21,8 +21,7 @@ function(init_target target_name) # init_target(my_target folder_name)
         XCODE_ATTRIBUTE_GCC_INLINES_ARE_PRIVATE_EXTERN YES
         XCODE_ATTRIBUTE_GCC_SYMBOLS_PRIVATE_EXTERN YES
     )
-    if (TG_OWT_PACKAGED_BUILD)
-    else()
+    if (NOT TG_OWT_SPECIAL_TARGET STREQUAL "")
         set_target_properties(${target_name} PROPERTIES
             XCODE_ATTRIBUTE_GCC_OPTIMIZATION_LEVEL $<IF:$<CONFIG:Debug>,0,fast>
             XCODE_ATTRIBUTE_LLVM_LTO $<IF:$<CONFIG:Debug>,NO,YES>
@@ -68,7 +67,7 @@ function(init_target target_name) # init_target(my_target folder_name)
                 -Wno-narrowing
                 -Wno-return-type
             )
-            if (CMAKE_SIZEOF_VOID_P EQUAL 4)
+            if (NOT TG_OWT_SPECIAL_TARGET STREQUAL "" AND CMAKE_SIZEOF_VOID_P EQUAL 4)
                 target_compile_options(${target_name}
                 PRIVATE
                     -g0
