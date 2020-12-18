2f8ea42d89b71754c7fb0c3040b93aea3f760a88
John Preston <johnprestonmail@gmail.com>
2020-12-18 15:59:35 +0400

    Patch: Add 'guid' selection for AudioDeviceMac.

--- src/modules/audio_device/mac/audio_device_mac.h
+++ src/modules/audio_device/mac/audio_device_mac.h
@@ -179,7 +179,8 @@ class AudioDeviceMac : public AudioDeviceGeneric {
 
   int32_t GetDeviceName(AudioObjectPropertyScope scope,
                         uint16_t index,
-                        char* name);
+                        char* name,
+                        char* guid);
 
   int32_t InitDevice(uint16_t userDeviceIndex,
                      AudioDeviceID& deviceId,
