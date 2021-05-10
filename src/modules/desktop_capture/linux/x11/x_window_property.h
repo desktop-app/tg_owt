decf9b382b8eb3a76ab943b15b6b8c334fd35c03
John Preston <johnprestonmail@gmail.com>
2021-05-10 08:24:32 +0000

    Patch: Fix XGetWindowProperty usage on 64 bit systems.
    On 64 bit systems for the format '32' the values are 64 bit, not 32 bit.
    
    So for each value in the list we need to read one value and skip one value.
    
    See https://www.x.org/releases/X11R7.5/doc/man/man3/XChangeProperty.3.html
    
    ---
    If the returned format is 32, the property data will be stored as
    an array of longs (which in a 64-bit application will be 64-bit values
    that are padded in the upper 4 bytes).
    ---

--- src/modules/desktop_capture/linux/x11/x_window_property.h
+++ src/modules/desktop_capture/linux/x11/x_window_property.h
@@ -14,6 +14,9 @@
 #include <X11/X.h>
 #include <X11/Xlib.h>
 
+#include <type_traits>
+#include <vector>
+
 namespace webrtc {
 
 class XWindowPropertyBase {
@@ -41,21 +44,74 @@ class XWindowPropertyBase {
   unsigned long size_ = 0;  // NOLINT: type required by XGetWindowProperty
 };
 
+
 // Convenience wrapper for XGetWindowProperty() results.
 template <class PropertyType>
-class XWindowProperty : public XWindowPropertyBase {
+class XWindowPropertyGeneric : public XWindowPropertyBase {
  public:
-  XWindowProperty(Display* display, const Window window, const Atom property)
+  XWindowPropertyGeneric(Display* display, const Window window, const Atom property)
       : XWindowPropertyBase(display, window, property, sizeof(PropertyType)) {}
-  ~XWindowProperty() override = default;
+  ~XWindowPropertyGeneric() override = default;
 
-  XWindowProperty(const XWindowProperty&) = delete;
-  XWindowProperty& operator=(const XWindowProperty&) = delete;
+  XWindowPropertyGeneric(const XWindowPropertyGeneric&) = delete;
+  XWindowPropertyGeneric& operator=(const XWindowPropertyGeneric&) = delete;
 
   const PropertyType* data() const {
     return reinterpret_cast<PropertyType*>(data_);
   }
   PropertyType* data() { return reinterpret_cast<PropertyType*>(data_); }
+};
+
+// See 'XGetWindowProperty' documentation. On 64-bit systems it returns array of 64 bit values:
+//
+// "If the returned format is 32, the property data will be stored as an array of longs
+// (which in a 64-bit application will be 64-bit values that are padded in the upper 4 bytes)."
+template <class PropertyType>
+class XWindowProperty32On64 : public XWindowPropertyBase {
+ public:
+  XWindowProperty32On64(Display* display, const Window window, const Atom property)
+      : XWindowPropertyBase(display, window, property, sizeof(PropertyType)) {}
+  ~XWindowProperty32On64() override = default;
+
+  XWindowProperty32On64(const XWindowProperty32On64&) = delete;
+  XWindowProperty32On64& operator=(const XWindowProperty32On64&) = delete;
+
+  const PropertyType* data() const {
+    fill_real();
+    return real_.data();
+  }
+  PropertyType* data() {
+    fill_real();
+    return real_.data();
+  }
+
+ private:
+  void fill_real() const {
+    if (!real_.empty() || !is_valid() || !size()) {
+      return;
+    }
+    real_.resize(size());
+    PropertyType *values = reinterpret_cast<PropertyType*>(data_);
+    for (PropertyType &value : real_) {
+      value = *values;
+      values += 2;
+    }
+  }
+  mutable std::vector<PropertyType> real_;
+};
+
+template <typename PropertyType>
+using XWindowPropertyParent = std::conditional_t<
+    sizeof(PropertyType) == 4 && sizeof(void*) == 8,
+    XWindowProperty32On64<PropertyType>,
+    XWindowPropertyGeneric<PropertyType>>;
+
+template <typename PropertyType>
+class XWindowProperty : public XWindowPropertyParent<PropertyType> {
+  using Parent = XWindowPropertyParent<PropertyType>;
+public:
+  using Parent::Parent;
+  ~XWindowProperty() override = default;
 };
 
 }  // namespace webrtc
