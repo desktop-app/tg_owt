3954d3d80bc0f96d10c11357c30648e41bde7085
John Preston <johnprestonmail@gmail.com>
2022-05-04 09:17:46 +0400

    Patch: Don't try loading libdrm using stubs.

--- src/modules/desktop_capture/linux/wayland/shared_screencast_stream.cc
+++ src/modules/desktop_capture/linux/wayland/shared_screencast_stream.cc
@@ -31,7 +31,6 @@
 #if defined(WEBRTC_DLOPEN_PIPEWIRE)
 #include "modules/desktop_capture/linux/wayland/pipewire_stubs.h"
 using modules_desktop_capture_linux_wayland::InitializeStubs;
-using modules_desktop_capture_linux_wayland::kModuleDrm;
 using modules_desktop_capture_linux_wayland::kModulePipewire;
 using modules_desktop_capture_linux_wayland::StubPathMap;
 #endif  // defined(WEBRTC_DLOPEN_PIPEWIRE)
@@ -42,7 +41,6 @@ const int kBytesPerPixel = 4;
 
 #if defined(WEBRTC_DLOPEN_PIPEWIRE)
 const char kPipeWireLib[] = "libpipewire-0.3.so.0";
-const char kDrmLib[] = "libdrm.so.2";
 #endif
 
 constexpr int kCursorBpp = 4;
@@ -382,13 +380,11 @@ bool SharedScreenCastStreamPrivate::StartScreenCastStream(
 
   // Check if the PipeWire and DRM libraries are available.
   paths[kModulePipewire].push_back(kPipeWireLib);
-  paths[kModuleDrm].push_back(kDrmLib);
 
   if (!InitializeStubs(paths)) {
     RTC_LOG(LS_ERROR)
         << "One of following libraries is missing on your system:\n"
-        << " - PipeWire (" << kPipeWireLib << ")\n"
-        << " - drm (" << kDrmLib << ")";
+        << " - PipeWire (" << kPipeWireLib << ")";
     return false;
   }
 #endif  // defined(WEBRTC_DLOPEN_PIPEWIRE)
