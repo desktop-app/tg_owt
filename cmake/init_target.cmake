609b9821a71fb51e3aa5ecf9083ac5b6ef7718c7
Ilya Fedin <fedin-ilja2010@ya.ru>
2025-05-01 14:47:13 +0400

    CMake: Don't force MSVC runtime library

--- cmake/init_target.cmake
+++ cmake/init_target.cmake
@@ -20,7 +20,6 @@ function(init_target target_name) # init_target(my_target folder_name)
     )
     if (NOT TG_OWT_SPECIAL_TARGET STREQUAL "")
         set_target_properties(${target_name} PROPERTIES
-            MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>"
             XCODE_ATTRIBUTE_GCC_OPTIMIZATION_LEVEL $<IF:$<CONFIG:Debug>,0,fast>
             XCODE_ATTRIBUTE_LLVM_LTO $<IF:$<CONFIG:Debug>,NO,YES>
         )
