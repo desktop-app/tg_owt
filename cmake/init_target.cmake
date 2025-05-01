2971c2195b2238809b77a0737efcb87c64d3944d
Ilya Fedin <fedin-ilja2010@ya.ru>
2025-05-01 14:48:59 +0400

    CMake: Don't duplicate compile options with target properties
    XCODE_ATTRIBUTE_CLANG_ENABLE_OBJC_WEAK: -fobjc-arc
    XCODE_ATTRIBUTE_GCC_INLINES_ARE_PRIVATE_EXTERN: -fvisibility-inlines-hidden
    XCODE_ATTRIBUTE_GCC_SYMBOLS_PRIVATE_EXTERN: -fvisibility=hidden

--- cmake/init_target.cmake
+++ cmake/init_target.cmake
@@ -13,11 +13,6 @@ endfunction()
 function(init_target target_name) # init_target(my_target folder_name)
     target_compile_features(${target_name} PUBLIC cxx_std_20)
 
-    set_target_properties(${target_name} PROPERTIES
-        XCODE_ATTRIBUTE_CLANG_ENABLE_OBJC_WEAK YES
-        XCODE_ATTRIBUTE_GCC_INLINES_ARE_PRIVATE_EXTERN YES
-        XCODE_ATTRIBUTE_GCC_SYMBOLS_PRIVATE_EXTERN YES
-    )
     if (NOT TG_OWT_SPECIAL_TARGET STREQUAL "")
         set_target_properties(${target_name} PROPERTIES
             XCODE_ATTRIBUTE_GCC_OPTIMIZATION_LEVEL $<IF:$<CONFIG:Debug>,0,fast>
