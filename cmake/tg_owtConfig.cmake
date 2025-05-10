8c233a4c12d6ec1f2aa87991564ac28cc996c57a
Ilya Fedin <fedin-ilja2010@ya.ru>
2025-05-11 03:17:11 +0400

    CMake: Don't use REQUIRED with find_dependency
    It stops the execution of config file by default while with REQUIRED it stops execution of the entire cmake configuration

--- cmake/tg_owtConfig.cmake
+++ cmake/tg_owtConfig.cmake
@@ -1,9 +1,9 @@
 include(CMakeFindDependencyMacro)
 if (@absl_FOUND@)
-    find_dependency(absl REQUIRED)
+    find_dependency(absl)
 endif()
 if (@Crc32c_FOUND@)
-    find_dependency(Crc32c REQUIRED)
+    find_dependency(Crc32c)
 endif()
 
 include("${CMAKE_CURRENT_LIST_DIR}/tg_owtTargets.cmake")
